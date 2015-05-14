3a4fafccef36cdc8e9d328b5900096d09ca0ae53
ARTVIRTUAL <thiagoncg1@gmail.com>
2015-05-14 00:39:56 -0300

    Atualizado Game

--- PhraseDay/Assets/Scripts/SplashScreen.cs
+++ PhraseDay/Assets/Scripts/SplashScreen.cs
@@ -3,21 +3,11 @@ using System.Collections;
 
 public class SplashScreen : MonoBehaviour {
 
-	public float delaytimer = 5;
-
+	public float delaytimer = 4;
 	IEnumerator Start(){
+
 		yield return new WaitForSeconds (delaytimer);
-
 		Application.LoadLevel (1);
-		}
 
-	/*/ Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}*/
+		}
 }
